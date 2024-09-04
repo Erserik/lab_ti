b5e5641516b40efa19f0d2a019c73fe531637432
Erserik <74207247+Erserik@users.noreply.github.com>
2024-09-04 14:29:34 +0500

    lab1

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+import string
+from collections import Counter
+import math
+
+s = list(string.ascii_letters)
+with open('r1.txt', 'w') as file:
+    random_string = ''.join(random.choices(s, k=2000))
+    file.write(random_string)
+
+b = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+with open('r2.txt', 'w') as file:
+    random_digits = ''.join(random.choices(b, k=2000))
+    file.write(random_digits)
+
+counter_r1 = Counter(random_string)
+counter_r2 = Counter(random_digits)
+
+prob_r1 = {char: count/len(random_string) for char, count in counter_r1.items()}
+prob_r2 = {char: count/len(random_digits) for char, count in counter_r2.items()}
+
+en_r1 = {char: prob * math.log2(prob) for char, prob in prob_r1.items()}
+en_r2 = {char: prob * math.log2(prob) for char, prob in prob_r2.items()}
+
+print("r1:", counter_r1)
+print("r1:", prob_r1)
+print("P(xi) * log2(P(xi)) r1:", en_r1)
+print("sum P(xi) * log2(P(xi)) r1:", sum(en_r1.values()))
+
+print("r2:", counter_r2)
+print("r2:", prob_r2)
+print("P(xi) * log2(P(xi)) r2:", en_r2)
+print("sum P(xi) * log2(P(xi)) r2:", sum(en_r2.values()))
---
+++ main2.py
@@ -0,0 +1,28 @@
+from collections import Counter
+import math
+import matplotlib.pyplot as plt
+
+
+def calculate_entropy(file_name):
+    with open(file_name, 'r') as file:
+        text = file.read()
+    text = ''.join(char for char in text if char.isprintable())
+
+    counter = Counter(text)
+    prob = {char: count / len(text) for char, count in counter.items()}
+    entropy_values = {char: prob_value * math.log2(prob_value) for char, prob_value in prob.items()}
+    iz = ((math.log2(len(prob)) - (-sum(entropy_values.values()))) / math.log2(len(prob))) * 100
+
+    print("Count: ", counter)
+    print("Probabilities: ", prob)
+    print("P(xi) * log2(P(xi)): ", entropy_values)
+    print("Sum P(xi) * log2(P(xi)): ", (-sum(entropy_values.values())))
+    print("Избыточность: ", iz)
+
+    plt.bar(prob.keys(), prob.values())
+    plt.xlabel('Characters')
+    plt.ylabel('Probability')
+    plt.show()
+
+
+calculate_entropy('r6.txt')
