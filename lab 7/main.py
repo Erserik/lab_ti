c10cb8c246c4307e9e00eaa7cd14797b2c7aa967
sanches21pm <krasnozhonov.210120045@gmail.com>
2024-10-30 14:43:46 +0500

    lab 7

--- lab 7/main.py
+++ lab 7/main.py
@@ -1,37 +1,39 @@
 from collections import Counter
-def calculate(data):
-    frequency = Counter(data)
-    total_symbols = len(data)
-    probabilities = {char: count / total_symbols for char, count in frequency.items()}
-    return sorted(probabilities.items(), key=lambda x: x[1], reverse=True)
 
 
-def shannon_fano_coding(symbols, prefix=''):
+def calc_probs(data):
+    freq = Counter(data)
+    total = len(data)
+    probs = {char: count / total for char, count in freq.items()}
+    return sorted(probs.items(), key=lambda x: x[1], reverse=True)
+
+
+def shannon_fano(symbols, prefix=''):
     if len(symbols) == 1:
         return {symbols[0][0]: prefix}
 
     total_prob = sum(prob for _, prob in symbols)
-    cumulative_prob = 0
-    split_index = 0
+    cum_prob = 0
+    split_idx = 0
     for i, (_, prob) in enumerate(symbols):
-        cumulative_prob += prob
-        if cumulative_prob >= total_prob / 2:
-            split_index = i + 1
+        cum_prob += prob
+        if cum_prob >= total_prob / 2:
+            split_idx = i + 1
             break
 
-    left_group = symbols[:split_index]
-    right_group = symbols[split_index:]
+    left = symbols[:split_idx]
+    right = symbols[split_idx:]
 
     code = {}
-    code.update(shannon_fano_coding(left_group, prefix + '0'))
-    code.update(shannon_fano_coding(right_group, prefix + '1'))
+    code.update(shannon_fano(left, prefix + '0'))
+    code.update(shannon_fano(right, prefix + '1'))
     return code
 
 
-message = "ABCABEAAFB"
-symbols = calculate("ABCABEAAFB")
+msg = "ABCABEAAFB"
+symbols = calc_probs(msg)
 print(symbols)
-code_map = shannon_fano_coding(symbols)
+code_map = shannon_fano(symbols)
 print(code_map)
-encoded_message = ''.join(code_map[char] for char in message)
-print(encoded_message)
+encoded_msg = ''.join(code_map[char] for char in msg)
+print(encoded_msg)
